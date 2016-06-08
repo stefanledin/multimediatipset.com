a3bee19ab1a6a6c1671b1634b075e316bc7aafd9
Stefan Ledin <stefan.ledin@gmail.com>
2016-06-08 16:16:21 +0200

    Avslutade tips visas

--- resources/views/games/index.blade.php
+++ resources/views/games/index.blade.php
@@ -5,6 +5,7 @@
 		<div class="col s12 m6 offset-m3">
 			<div class="card orange darken-3">
 				<div class="card-content">
+					
 					@if(count($active_games))
 					<span class="card-title">Aktuella tips</span>
 					<div class="collection with-header">
@@ -19,6 +20,19 @@
 					<span class="card-title">Inga pågående tips</span>
 					<br>
 					@endif
+					
+					@if(count($finished_games))
+					<span class="card-title">Avslutade tips</span>
+					<div class="collection with-header">
+						@foreach($finished_games as $game)
+							<a href="games/{{ $game->id }}" class="orange-text collection-item">
+								{{ $game->name }}
+								<span class="badge">{{ count($game->predictions) }} tips</span>
+							</a>
+						@endforeach
+					</div>
+					@endif
+
 					@if (Auth::check() && Auth::user()->is_admin)
 					<a href="{{ route('games.create') }}" class="btn orange">Nytt</a>
 					@endif
