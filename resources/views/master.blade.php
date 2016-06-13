3f170be5b260e07c183ed0f3fdc369b42ef804db
Stefan Ledin <stefan.ledin@gmail.com>
2016-06-13 13:59:21 +0200

    analytics!

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -26,5 +26,18 @@
 		@show
 
 		<script type="text/javascript" src="/js/all.js"></script>
+
+		@if(App::environment() != 'local')
+		<script>
+		(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+		(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+		m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+		})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+		ga('create', 'UA-28191752-3', 'auto');
+		ga('send', 'pageview');
+		</script>
+		@endif
+
 	</body>
 </html>
