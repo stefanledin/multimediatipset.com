6dea19f1ca37061dced44fa513f208def4910e82
Stefan Ledin <stefan.ledin@gmail.com>
2016-09-15 15:36:48 +0200

    grejat vidare

---
+++ app/Answer/Save/Type/Alternatives.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Answer\Save\Type;
+
+use App\Answer;
+use App\Question;
+use \Auth;
+
+class Alternatives {
+    
+    public function __construct($request)
+    {
+        $this->store($request->input('answer'));
+    }
+    
+    public function store($answers)
+    {
+        foreach ($answers as $questionID => $answer) {
+            $answer = new Answer([
+                'answer' => $answer
+            ]);
+            $question = Question::find($questionID);
+            $question->answers()->save($answer);
+            Auth::user()->answers()->save($answer);
+        }    
+    }
+
+}
---
+++ app/Validators/AlternativesAnswerValidator.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Validators;
+
+use Illuminate\Http\Request;
+use \App\Question;
+
+class AlternativesAnswerValidator {
+
+    protected $request;
+
+    function __construct(Request $request)
+    {
+        $this->request = $request;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        $rules = [];
+        foreach ($this->request->get('answer') as $questionID => $value) {
+            $rules['answer.'.$questionID] = 'required';
+        }
+        return $rules;
+    }
+
+    /**
+     * Get the error messages for the defined validation rules.
+     *
+     * @return array
+     */
+    public function messages()
+    {
+        $messages = [];
+        foreach ($this->request->get('answer') as $questionID => $value) {
+            $question = Question::find($questionID);
+            $messages['answer.'.$questionID.'.required'] = 'Tipset för '.$question->title.' saknas';
+        }
+        return $messages;   
+    }
+
+}
