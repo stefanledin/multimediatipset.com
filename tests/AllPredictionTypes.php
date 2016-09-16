3ad53793f1d8d3379837bab42269e1f8a180ad9a
Stefan Ledin <stefan.ledin@gmail.com>
2016-09-16 16:47:28 +0200

    grejat vidare

--- tests/AllPredictionTypes.php
+++ tests/AllPredictionTypes.php
@@ -52,17 +52,57 @@ class AllPredictionTypes extends TestCase
         ]);
         $game->questions()->saveMany([$question1, $question2, $question3]);
 
+        // User1
         $user1 = factory(App\User::class)->create(['is_admin' => false]);
-        $user2 = factory(App\User::class)->create(['is_admin' => false]);
-
+        // Rätt. 1p
         $user1_question1_answer = new Answer([
             'answer' => '1'
         ]);
+        // Fel. 0p
         $user1_question2_answer = new Answer([
             'answer' => '5-2'
         ]);
+        // Alla rätt. 12 poäng
         $user1_question3_answer = new Answer([
             'answer' => ['Ryssland', 'Sverige', 'Finland', 'Team Europe']
         ]);
+        // 13 poäng av 18 möjliga
+        $user1->answers()->saveMany([$user1_question1_answer, $user1_question2_answer, $user1_question3_answer]);
+        
+        // User 2
+        $user2 = factory(App\User::class)->create(['is_admin' => false]);
+        // Fel. 0p
+        $user2_question1_answer = new Answer([
+            'answer' => '2'
+        ]);
+        // Rätt. 5p
+        $user2_question2_answer = new Answer([
+            'answer' => '6-3'
+        ]);
+        // Två rätt. 2p
+        $user2_question3_answer = new Answer([
+            'answer' => ['Sverige', 'Ryssland', 'Finland', 'Team Europe']
+        ]);
+        // 7 poäng av 18 möjliga
+        $user2->answers()->saveMany([$user2_question1_answer, $user2_question2_answer, $user2_question3_answer]);
+
+        // Spara frågorna
+        $question1->answers()->saveMany([$user1_question1_answer, $user2_question1_answer]);
+        $question2->answers()->saveMany([$user1_question2_answer, $user2_question2_answer]);
+        $question3->answers()->saveMany([$user1_question3_answer, $user2_question3_answer]);
+
+        // Rätta svaren, user1
+        $this->assertEquals(1, $user1_question1_answer->points());
+        $this->assertEquals(0, $user1_question2_answer->points());
+        $this->assertEquals(14, $user1_question3_answer->points());
+        // Rätta svaren, user2
+        $this->assertEquals(0, $user2_question1_answer->points());
+        $this->assertEquals(5, $user2_question2_answer->points());
+        $this->assertEquals(2, $user2_question3_answer->points());
+
+        // game
+        $this->assertEquals(15, $game->leaderBoard()->players[0]->points);
+        $this->assertEquals(7, $game->leaderBoard()->players[1]->points);
+        $this->assertEquals(20, $game->pointsAvaliable());
     }
 }
